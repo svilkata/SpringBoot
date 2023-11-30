8d05a800ea99e9a67e2c268c4b7aeb2c7d60fca1
Svilen Velikov <svilen.veikov@gmail.com>
2023-11-30 12:42:40 +0200

    NoArgsConstructor lombok added

--- DataWithSpringJdbc/src/main/java/bg/jug/academy/DataWithSpringJdbc/module/EmployeeEntity.java
+++ DataWithSpringJdbc/src/main/java/bg/jug/academy/DataWithSpringJdbc/module/EmployeeEntity.java
@@ -2,18 +2,15 @@ package bg.jug.academy.DataWithSpringJdbc.module;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeEntity {
-
     private String id;
     private String name;
     private Long salary;
-
-    public EmployeeEntity() {
-    }
-
 }
